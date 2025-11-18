481e9ccfe49a4dae8cf814e42a3e7fa43c5ee7c2
Skfufu <enzo.carapuce2011@gmail.com>
2025-11-18 23:34:46 +0100

    Fix kick command syntax and variable names

--- testdiscordbot/commands/kick.py
+++ testdiscordbot/commands/kick.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord import app_commands
+from discord import app_commands, Interaction
 import discord
 import asyncio #note : from discord import Message, app_commands, user
 from discord.ext import commands
@@ -10,29 +10,30 @@ class kickcommande(commands.cog):
         self.bot = bot
     
 @app_commands.command(name= "kick", description= "kick les personne")
-async def kick(self, interactions, user: discord.Member, time: int, raison: str = "aucune raison"):
+async def kick(self, interaction: Interaction user: discord.Member, time: int, raison: str = "aucune raison"):
         await interactions.response.defter(epherale=True)
 
-        has_permission = interactions.user.guild_permissions.kick_members
+        has_permission = interaction.user.guild_permissions.kick_members
         if not has_permission:
             await interactions.followup.send("vous n'avez pas les permission")
             return
         
         if time <= 0 or time > 20160:
-            await interactions.followup.send(f"{time} est beaucoup trop grand ou trop petit")    
+            await interaction.followup.send(f"{time} est beaucoup trop grand ou trop petit")    
             return
         
         try:
-             await interactions.guild.ban(user, reason=raison, delete_message_days=time)
-             await interactions.response.send_message(f"{user} a été exclue pour {raison} pendant {time}")
+             await interaction.guild.ban(user, reason=raison, delete_message_days=time)
+             await interaction.response.send_message(f"{user} a été exclue pour {raison} pendant {time}")
 
              await asyncio.sleep(time *60)
-             await interactions.guild.unban(user, reason= "fin du temps d'exclusion")
+             await interaction.guild.unban(user, reason= "fin du temps d'exclusion")
         
         except Exception as e:
-             await interactions.response.send_message(f"{user} na pas pue etre exclue {e}")
+             await interaction.response.send_message(f"{user} na pas pue etre exclue {e}")
         
 
         
+
 
 
