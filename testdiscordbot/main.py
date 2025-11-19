265a32f8e095dfe1ad5bfbe034cb44317dee228c
Skfufu <enzo.carapuce2011@gmail.com>
2025-11-19 16:48:05 +0100

    Refactor main.py to simplify bot setup
    Removed unused imports and streamlined setup_hook method.

--- testdiscordbot/main.py
+++ testdiscordbot/main.py
@@ -1,18 +1,5 @@
 from dotenv import load_dotenv
 from discord.ext import commands
-from commands.ban import Bancommand
-from commands.clear import ClearCommand
-from commands.kick import kickcommande
-from Message.BlackList import blacklist
-from Message.jeux_scp import jeuxscp
-from Message.cmds_infos import infoscommands
-from Message.infosxeroze import xeroze
-from Message.mapper import mapper
-from Message.dev import dev
-from Message.lepoteaufeu import lepoteaufeu
-from commands.deban import deBancommand
-from alive import alive
-from commands.muet import MuetCommand
 import discord
 import os
 
@@ -27,18 +14,6 @@ class Mybot(commands.Bot):
         super().__init__(command_prefix=prefix, intents=intents)
     
     async def setup_hook(self):
-        await self.add_cog(ClearCommand)
-        await bot.add_cog(Bancommand)
-        await bot.add_cog(kickcommande)
-        await self.add_cog(blacklist)
-        await self.add_cog(jeuxscp)
-        await self.add_cog(infoscommands)
-        await self.add_cog(xeroze)
-        await self.add_cog(mapper)
-        await self.add_cog(dev)
-        await self.add_cog(lepoteaufeu)
-        await self.add_cog(deBancommand)
-        await self.add_cog(MuetCommand)
         await self.tree.sync()
 
     
@@ -53,3 +28,4 @@ bot = Mybot()
 
 
 bot.run(token)
+
