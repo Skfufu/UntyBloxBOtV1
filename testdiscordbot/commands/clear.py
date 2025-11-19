6c7f08682314657c4062437d042403c0d5dc3b7d
Skfufu <enzo.carapuce2011@gmail.com>
2025-11-19 17:46:36 +0100

    Update clear.py

--- testdiscordbot/commands/clear.py
+++ testdiscordbot/commands/clear.py
@@ -1,27 +1,29 @@
 from discord.ext import commands
 from discord import app_commands, Interaction
-import discord 
-
+import discord
 
 class ClearCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="clear", description="Supprime le nombre de messages spécifié")
-    async def clear(self, interaction: discord.Interaction, number: int):
+    async def clear(self, interaction: Interaction, number: int):
         await interaction.response.defer(ephemeral=True)
         
-        has_permission = interaction.user.guild_permissions.manage_messages
-        if not has_permission:
+        if not interaction.user.guild_permissions.manage_messages:
             await interaction.followup.send("Vous n'avez pas la permission de gérer les messages.", ephemeral=True)
             return
 
         if number <= 0 or number > 100:
             await interaction.followup.send("Le nombre de messages à supprimer doit être entre 1 et 100.", ephemeral=True)
             return
-        
-        deleted = await interaction.channel.purge(limit=number)
-        await interaction.followup.send(f"{len(deleted)} messages supprimés", ephemeral=True)
+
+        channel = interaction.channel
+        if isinstance(channel, discord.TextChannel):
+            deleted = await channel.purge(limit=number)
+            await interaction.followup.send(f"{len(deleted)} messages supprimés", ephemeral=True)
+        else:
+            await interaction.followup.send("Impossible de supprimer les messages ici.", ephemeral=True)
 
 
 
