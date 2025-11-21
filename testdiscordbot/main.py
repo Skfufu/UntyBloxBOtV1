74f42490fd153b86f83a123acdfd6199686db672
Skfufu <enzo.carapuce2011@gmail.com>
2025-11-21 21:06:43 +0100

    Update main.py

--- testdiscordbot/main.py
+++ testdiscordbot/main.py
@@ -1,30 +1,30 @@
+# main.py
 import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
-# Charger les variables d'environnement depuis le .env
+# Charger les variables d'environnement
 load_dotenv()
 token = os.getenv("tokendiscord")
 if token is None:
     raise ValueError("tokendiscord n'est pas défini dans les variables d'environnement !")
 
-# Définir les extensions à charger
+# Liste des extensions
 COMMAND_EXTENSIONS = ['Bancommand', 'ClearCommand', 'kickcommande', 'deBancommand', 'MuetCommand']
 MESSAGE_EXTENSIONS = ['infoscommands', 'xeroze', 'mapper', 'dev', 'lepoteaufeu', 'alive']
 
-# Création du bot
 class MonBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.all()
         super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
-        # Charger les extensions du dossier commands
+        # Charger les extensions commands
         for ext in COMMAND_EXTENSIONS:
             await self.load_extension(f'testdiscordbot.commands.{ext}')
 
-        # Charger les extensions du dossier Message
+        # Charger les extensions Message
         for ext in MESSAGE_EXTENSIONS:
             await self.load_extension(f'testdiscordbot.Message.{ext}')
 
@@ -33,9 +33,6 @@ bot = MonBot()
 
 # Lancer le bot
 bot.run(token)
-
-
-
 
 
 
